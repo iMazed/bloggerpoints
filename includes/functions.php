ab8a0342f8af71bc82082848c710fffb9e035ac5
Ines <ines@programmeurster.nl>
2016-09-28 14:32:00 +0200

    Clean up functions.php
    Restructure/rename a bit, make sure everything is up to standard. Still a mess, but a little less than it was before.

--- includes/functions.php
+++ includes/functions.php
@@ -6,12 +6,12 @@
 /**
  * Retrieve total word count for user
  */
-class word_count {
+class BP_Word_Count {
 
 	/**
 	 * Retrieve all meta values
 	 */
-	public function get_meta_values( $key = '', $type = 'post', $status = 'publish', $user_id = '' ) {
+	public function bp_get_meta_values( $key = '', $type = 'post', $status = 'publish', $user_id = '' ) {
 
 		global $wpdb;
 
@@ -35,14 +35,14 @@ class word_count {
 	 * Add up total word count for current user
 	 * @return mixed
 	 */
-	public function word_count_total() {
+	public function bp_word_count_total() {
 
 		/* Set ID for current user */
 		$current_user       = wp_get_current_user();
 		$current_user_id    = $current_user->ID;
 
 		/* Retrieve word count of all posts that current user has published */
-		$all_post = $this->get_meta_values('word_count', 'post', 'publish', $current_user_id);
+		$all_post = $this->bp_get_meta_values('word_count', 'post', 'publish', $current_user_id);
 
 		/* Add all integers to get to a total */
 		$count_all_post = array_sum($all_post);
