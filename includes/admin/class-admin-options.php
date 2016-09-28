2fe525156d6e2d396acbfb98bc5148f03d9acd2c
Ines <ines@programmeurster.nl>
2016-09-28 14:30:18 +0200

    Clean up admin
    Cleaning up a bit, making sure all echoed strings are translatable.

--- includes/admin/class-admin-options.php
+++ includes/admin/class-admin-options.php
@@ -3,20 +3,20 @@
  * Admin page for Bloggerpoints
  */
 
-add_action('admin_menu', 'bloggerpoints_setup_menu');
+add_action('admin_menu', 'bp_setup_menu');
 
-function bloggerpoints_setup_menu(){
+function bp_setup_menu(){
 	/**
 	 * Add wp-admin menu page
 	 */
 	// Add an item to the menu.
 	add_menu_page(
-		__( 'Wp Bloggerpoints', 'bloggerpoints' ),
+		__( 'WP Bloggerpoints', 'bloggerpoints' ),
 		__( 'WP Bloggerpoints', 'bloggerpoints' ),
 		'manage_options',
 		'bloggerpoints',
 		'bloggerpoints_init',
-		'dashicons-admin-page'
+		'dashicons-admin-customizer'
 	);
 }
 
@@ -25,14 +25,20 @@ function bloggerpoints_init(){
 	<div class="wrap">
 		<h1 class="page-title">WP Bloggerpoints</h1>
 		<hr>
-		<h3>Your Writing Stats</h3>
+		<h3><?php _e('Your Writing Stats', 'bloggerpoints'); ?></h3>
 		<p>
-		<strong>Your current word count:</strong>
+		<strong><?php _e('Your current word count:', 'bloggerpoints'); ?></strong>
 		<?php
-			$word_count = new word_count();
-			echo $word_count->word_count_total();
+			$word_count = new BP_Word_Count();
+			echo $word_count->bp_word_count_total();
 		?>
 		</p>
+		<hr>
+		<h3><?php _e("Your Badges", "bloggerpoints"); ?></h3>
+		<?php
+			$badge = new Badge();
+			echo $badge->bp_assign_badge();
+		?>
 	</div>
 
 <?php
